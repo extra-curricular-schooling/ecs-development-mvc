3c65b7b7db714eb4ffd2ee6abe1269e59b8709a8
Scott Roberts <s.e.roberts0@gmail.com>
2018-02-01 18:41:11 -0800

    Added RegistrationRequest Vue Component

---
+++ src/components/RegistrationAppToPortal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id='RegAppToPortalRequest'>
+    <button v-on:click='submit'>
+      Hit me!
+    </button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'RegAppToPortalRequest',
+  data () {
+    return {
+      msg: 'This is a request'
+    }
+  },
+  created () {},
+  methods: {
+    submit: (form) => {
+      axios.post('/registration-response', {
+        // hello world
+      })
+        // Not sure on the naming for this path...
+        .then(response => {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
