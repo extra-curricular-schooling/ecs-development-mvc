a12b70b50e902326b4c5285807ad4c3761c45999
Scott Roberts <s.e.roberts0@gmail.com>
2018-02-04 16:43:01 -0800

    Adding Garbage Classes
    The controller folder and models folder was missing, so I added garbage classes to test the upload.

---
+++ Server/ecs-dev-server/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+// This is a garbage Controller. Throw away after uploading to GitHub
+
+namespace ecs_dev_server.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
