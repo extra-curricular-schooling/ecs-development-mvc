908df6af6ce06cb10bce8b853196d2af08ae5ca4
Trisha <piechual@gmail.com>
2018-02-15 22:55:40 -0800

    Registration UI (update)
    Added Mailing Address and Security Qs fields.  Improved formatting issues.  Added other details.

--- Client/src/components/RegistrationForm.vue
+++ Client/src/components/RegistrationForm.vue
@@ -1,34 +1,71 @@
 <template>
   <form>
-    <div class="container">
-      <p>Please register your information to get started.</p>
-      <hr>
+    <p>Please register your information to get started.</p>
+    <fieldset>
+      <legend>Basic Info</legend>
+      <label for="firstname" class="required-data"><b>First Name</b></label><br>
+      <input type="text" placeholder="First Name" name="firstname" required><br>
 
-      <label for="firstname"><b>First Name</b></label><br>
-      <input type="text" placeholder="Jane" name="firstname" required><br>
+      <label for="lastname" class="required-data"><b>Last Name</b></label><br>
+      <input type="text" placeholder="Last Name" name="lastname" required><br>
 
-      <label for="lastname"><b>Last Name</b></label><br>
-      <input type="text" placeholder="Doe" name="lastname" required><br>
+      <label for="username" class="required-data"><b>Username</b></label><br>
+      <input type="text" placeholder="username" name="username" required><br>
 
-      <label for="username"><b>Username</b></label><br>
-      <input type="text" placeholder="janedoe123" name="username" required><br>
+      <label for="email" class="required-data"><b>Email Address</b></label><br>
+      <input type="text" placeholder="myemail@email.com" name="email" required><br>
 
-      <label for="email"><b>Email Address</b></label><br>
-      <input type="text" placeholder="janedoe123@email.com" name="email" required><br>
+      <label for="pswd" class="required-data"><b>Password</b></label><br>
+      <input type="password" placeholder="************" name="pswd" required><br>
 
-      <label for="pswd"><b>Password</b></label><br>
-      <input type="password" placeholder="********" name="pswd" required><br>
+      <label for="confirm-pswd" class="required-data"><b>Confirm Password</b></label><br>
+      <input type="password" placeholder="************" name="confirm-pswd" required><br>
+    </fieldset>
 
-      <label for="pswd_repeat"><b>Confirm Password</b></label><br>
-      <input type="password" placeholder="********" name="pswd_repeat" required><br>
+    <fieldset>
+      <legend>Mailing Address</legend>
+      <label for="street-address"><b>Street Address</b></label><br>
+      <input type="text" placeholder="Street Address" name="street-address" optional><br>
 
-      <p>By creating an account, you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+      <label for="city"><b>City</b></label><br>
+      <input type="text" placeholder="City" name="city" optional><br>
+
+      <label for="state"><b>State</b></label><br>
+      <input type="text" placeholder="State" name="state" optional><br>
+
+      <label for="zipcode"><b>Zip Code</b></label><br>
+      <input type="text" placeholder="Zip Code" name="zipcode" optional><br>
+    </fieldset>
+
+    <fieldset>
+      <legend>Security Questions</legend>
+      <select id="questions1" onchange="showQuestions1()">
+        <option value="0">--select--</option>
+      </select><label class="required-data"></label><br>
+      <input type="text" placeholder="Answer 1" name="answer1" required><br>
+
+      <select id="questions2" onchange="showQuestions2()">
+        <option value="0">--select--</option>
+      </select><label class="required-data"></label><br>
+      <input type="text" placeholder="Answer 2" name="answer2" required><br>
+
+      <select id="questions3" onchange="showQuestions3()">
+        <option value="0">--select--</option>
+      </select><label class="required-data"></label><br>
+      <input type="text" placeholder="Answer 3" name="answer3" required><br>
+    </fieldset>
+
+      <label>
+      <input type="checkbox" checked="checked" name="rememberme-box" style="margin-bottom:12px">Remember me</label><br>
+
+      <label>
+      <input type="checkbox" name="agreement-box" style="margin-bottom:12px">
+      I have read and agreed to the <a href="#" style="color:dodgerblue">Terms & Conditions</a>.</label>
 
       <div class="clearfix">
-        <button type="button" class="cancelbtn">Cancel</button>
-        <button type="submit" class="registerbtn">Register</button>
+        <button type="button" class="cancel-btn">Cancel</button>
+        <button class="register-btn" v-on:click.prevent="submit">Register</button>
       </div>
-    </div>
   </form>
 </template>
 
@@ -41,7 +78,7 @@ export default {
       // This is not a correct path... Please fix when available.
       axios({
         method: 'GET',
-        url: 'https://localhost/registration/SayHello',
+        url: 'https://localhost:44313/registration/SayHello',
         headers: {
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Credentials': true
@@ -55,31 +92,20 @@ export default {
 </script>
 
 <style>
-
-* {box-sizing: border-box}
-
-  input[type=text], input[type=password] {
-  width: 500px;
-  padding: 13px;
-  margin: 5px 0 30px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-  }
-
-  hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
-  }
+*, *:before, *:after {
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+}
 
   button {
-  background-color: #D3DCE5;
+  background-color: #E8E9E4;
   color: black;
   padding: 14px 20px;
   margin: 8px 0;
   border: none;
   cursor: pointer;
-  width: 250px;
+  width: 270px;
   opacity: 0.9;
   }
 
@@ -87,4 +113,45 @@ export default {
     opacity:1;
   }
 
+  fieldset {
+  margin-bottom: 10px;
+  border-color: #D3DCE5;
+  border-width:  5px;
+  background-color: #F8FCFB;
+  }
+
+  form {
+  max-width: 550px;
+  margin: 10px auto;
+  padding: 0px 0px;
+  border-radius: 8px;
+  text-align: left;
+  }
+
+  hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+  }
+
+  input[type=text], input[type=password] {
+  width: 500px;
+  padding: 10px;
+  margin: 5px 0 25px 0;
+  display: inline-block;
+  border: none;
+  background: #E8E9E4;
+  }
+
+  legend {
+  font-size: 1.4em;
+  }
+
+  label.required-data:after {
+  content: " *";
+  color: red;
+  }
+
+  p {
+  text-align: center;
+  }
 </style>
