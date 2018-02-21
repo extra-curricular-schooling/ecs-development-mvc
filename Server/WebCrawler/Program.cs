8b536343d73abc2724ec57639e9ab2f3fac78ce1
Hugo Garcia <hugogarcia354@gmail.com>
2018-02-21 10:12:21 -0800

    Push Web Crawler v0.9
    Pushing Web Crawler to Repo

---
+++ Server/WebCrawler/Program.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            TechCrawler techie = new TechCrawler();
+            ArtDesignCrawler artie = new ArtDesignCrawler();
+            EducationCrawler smartie = new EducationCrawler();
+            EarthCrawler earthie = new EarthCrawler();
+            HistoryCrawler oldie = new HistoryCrawler();
+            MedicalCrawler medie = new MedicalCrawler();
+            BusinessCrawler cachingie = new BusinessCrawler();
+            techie.CrawlingAsync().Wait();
+            artie.CrawlingAsync().Wait();
+            smartie.CrawlingAsync().Wait();
+            earthie.CrawlingAsync().Wait();
+            oldie.CrawlingAsync().Wait();
+            medie.CrawlingAsync().Wait();
+            cachingie.CrawlingAsync().Wait();
+            Console.WriteLine("\n"+DateTime.Now + ": Crawler Ended");
+
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
