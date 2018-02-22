2b5ca9bc4f4ca74ed43d627f16ab9bbd4ee724cf
FurkanArif <35707731+FurkanArif@users.noreply.github.com>
2018-02-21 18:00:49 -0800

    embed (v1.2)
    still working on it

--- Client/src/components/pages/Home.vue
+++ Client/src/components/pages/Home.vue
@@ -5,5 +5,37 @@
     <p><a href ="https://powerhookahus.com/" target="frame_result">Link 1</a></p>
     <p><a href ="https://www.hookah-shisha.com/" target="frame_result">Link 2</a></p>
     <p><a href ="http://www.ivc.edu/Pages/default.aspx" target="frame_result">Link 3 Education</a></p>
+    <vue-good-table :columns="columns" :onClick="onClickFn" :rows="rows" />
   </div>
 </template>
+<script>
+import Vue from 'vue'
+import VueGoodTable from 'vue-good-table'
+Vue.use(VueGoodTable)
+export default {
+  name: 'test',
+  data () {
+    return {
+      columns: [
+        {
+          label: 'Name',
+          field: 'name'
+        },
+        {
+          label: 'URL',
+          field: 'url'
+        }
+      ],
+      rows: [
+        {name: 'School', url: 'http://www.ivc.edu/Pages/default.aspx'},
+        {name: 'Hookah', url: 'https://www.hookah-shisha.com/'},
+        {name: 'Hookah', url: 'https://powerhookahus.com/'}
+      ],
+      onClickFn: function (row, index) {
+        console.log(row)
+        console.log(index)
+      }
+    }
+  }
+}
+</script>
